a4df2650b78a1301f087b278c37faf0a2368e3e5
Xanthrons <firamerga@gmail.com>
2026-04-29 17:16:01 +0300

    revised the screenshot logic and connect it with the telegram server

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+console.log("Full Env Object:", import.meta.env);
+console.log("My API URL is:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
